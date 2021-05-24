7f9b33dcf148c9dfe0a204e5848b9583f145c9ba
Beomjun Kang <kals323@gmail.com>
2021-05-24 20:32:55 +0900

    Fix broken tests

--- backend/user/tests/test_queries.py
+++ backend/user/tests/test_queries.py
@@ -2,6 +2,7 @@
 import json
 
 from parameterized import parameterized
+from rest_framework.authtoken.models import Token
 
 from backend.common.testcase import CustomGraphQLTestCase
 from backend.user.tests.factories import UserFactory
@@ -11,6 +12,7 @@ class TestUserQueries(CustomGraphQLTestCase):
 
     def setUp(self) -> None:
         self.user = UserFactory(email='new@test.com')
+        self.token = Token.objects.create(user=self.user)
 
         self.user_query = """
             query User($email: String!) {
@@ -25,7 +27,7 @@ class TestUserQueries(CustomGraphQLTestCase):
         """
 
     def test_user_query(self):
-        response = self.query(self.user_query, variables={'email': 'no_user@test.com'})
+        response = self.query_with_auth(self.user_query, self.user, self.token, variables={'email': 'no_user@test.com'})
         self.assertResponseNoErrors(response)
 
         content = json.loads(response.content)
@@ -34,7 +36,7 @@ class TestUserQueries(CustomGraphQLTestCase):
         result = content['data']['user']
         self.assertIsNone(result)
 
-        response = self.query(self.user_query, variables={'email': self.user.email})
+        response = self.query_with_auth(self.user_query, self.user, self.token, variables={'email': self.user.email})
         content = json.loads(response.content)
         self.assertIn('data', content)
 
@@ -47,7 +49,7 @@ class TestUserQueries(CustomGraphQLTestCase):
         ('isStaff',)
     ])
     def test_not_allowed_user_query(self, field: str):
-        response = self.query(
+        response = self.query_with_auth(
             """
             query User($email: String!) {
                 user(email: $email) {
@@ -55,6 +57,8 @@ class TestUserQueries(CustomGraphQLTestCase):
                 }
             }
             """.replace('{field}', field),
+            user=self.user,
+            token=self.token,
             op_name='user',
             variables={'email': self.user.email}
         )
